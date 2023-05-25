964a6119e2c28753984804cbe5154810551373dc
GinsengGhoul <ongteh@protonmail.com>
2023-05-24 23:12:29 -0700

    export public variables for use in 04-configuration

--- 03-pacstrap.sh
+++ 03-pacstrap.sh
@@ -12,14 +12,16 @@ microcode=
 # added thermald as it only works on intel as of 2023
 # https://github.com/intel/thermal_daemon/issues/383
 detect_microcode() {
+  if [ -z "$microcode" ]; then
   local vendor=$(cat /proc/cpuinfo | grep vendor_id | awk '{print $3}' | head -n1)
-  if [[ "$vendor" == "GenuineIntel" ]]; then
-    microcode="intel-ucode thermald"
-  elif [[ "$vendor" == "AuthenticAMD" ]]; then
-    microcode="amd-ucode"
-  else
-    # what cpu?
-    microcode=""
+    if [[ "$vendor" == "GenuineIntel" ]]; then
+      microcode="intel-ucode thermald"
+    elif [[ "$vendor" == "AuthenticAMD" ]]; then
+      microcode="amd-ucode"
+    else
+      # what cpu?
+      microcode=""
+    fi
   fi
 }
 
