48d8f16030a13e887267ed9cf7895b43d606b42c
GinsengGhoul <ongteh@protonmail.com>
2023-07-29 22:39:28 -0700

    updated zram udev rule based on new entry in arch wiki

--- 04-configuration.sh
+++ 04-configuration.sh
@@ -335,7 +335,7 @@ enable_zram() {
     # enable zram
     echo 'zram' >/mnt/etc/modules-load.d/zram.conf
     echo 'options zram num_devices=1' >/mnt/etc/modprobe.d/zram.conf
-    echo 'KERNEL=="zram0", ATTR{disksize}="'$(half_memory)'" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"' >/mnt/etc/udev/rules.d/99-zram.rules
+    echo 'ACTION=="add", KERNEL=="zram0", ATTR{comp_algorithm}="zstd", ATTR{disksize}="'$(half_memory)'", RUN="/usr/bin/mkswap -U clear /dev/%k", TAG+="systemd"' >/mnt/etc/udev/rules.d/99-zram.rules
 
     chmod 644 /mnt/etc/modules-load.d/zram.conf
     chmod 644 /mnt/etc/modprobe.d/zram.conf
