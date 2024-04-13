d6a4b0d84fb77155f6a9b3de7768482e3a36d043
GinsengGhoul <ongteh@protonmail.com>
2024-04-13 12:38:56 -0700

    fix stat cp error

--- 04-configuration.sh
+++ 04-configuration.sh
@@ -187,7 +187,8 @@ create_users() {
     arch-chroot /mnt mkdir -p /home/$username/.config
     arch-chroot /mnt cp -r /usr/share/goodies/scripts /home/$username/
     arch-chroot /mnt cp -r /usr/share/goodies/source /home/$username/
-    arch-chroot /mnt cp -r /usr/share/goodies/config/* /home/$username/.config
+    local copy="cp -r /usr/share/goodies/config/* /home/$username/.config"
+    arch-chroot /mnt $copy
     arch-chroot /mnt /bin/sh -c "chown -R $username /home/$username/scripts"
     arch-chroot /mnt /bin/sh -c "chown -R $username /home/$username/.config"
     arch-chroot /mnt /bin/sh -c "chmod 755 -R /home/$username/scripts"
