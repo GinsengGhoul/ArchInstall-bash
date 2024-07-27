e318bf22329dd9aa1b1cc982618e69b75d84eafa
GinsengGhoul <ongteh@protonmail.com>
2024-07-27 00:03:43 -0700

    updated to hangle download straglers

--- goodies/scripts/updateSystem.sh
+++ goodies/scripts/updateSystem.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sudo pacman -Syy && sudo powerpill -Su --noconfirm && paru -Syu --noconfirm --skipreview && paru -Sc --noconfirm
+sudo pacman -Syy && sudo powerpill -Su --noconfirm && paru -Syu --noconfirm --skipreview
+sudo rm -r /var/cache/pacman/pkg/*.part
+sudo rm -r /var/cache/pacman/pkg/*.aria2c
+paru -Sc --noconfirm
+~/scripts/updateISO.sh
