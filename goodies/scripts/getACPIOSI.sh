8ea8390bf12f9906c668e379cdd81bd482993857
GinsengGhoul <ongteh@protonmail.com>
2024-07-27 23:26:37 -0700

    added comments

--- goodies/scripts/getACPIOSI.sh
+++ goodies/scripts/getACPIOSI.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # add windows version closest to what would be insalled when sold to kernel line
 # eg. acpi_osi=! acpi_osi='Windows 2013'
+# acpi_backlight=video
+# acpi_backlight=vendor
+# acpi_backlight=native
+
 mkdir -p /tmp/acpi
 cd /tmp/acpi
 sudo acpidump -b
