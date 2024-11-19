c06ee082c097564e13a6938483760406ac5bccb5
GinsengGhoul <ongteh@protonmail.com>
2024-11-19 09:49:04 -0800

    update i3 config

--- goodies/config/i3/Scripts/SwitchMod1.sh
+++ goodies/config/i3/Scripts/SwitchMod1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 i3config="$HOME/.config/i3/config"
+i3move="$HOME/.config/i3/config.d/workspace"
 sed -i '/^#.*set \$mod Mod1/s/^#//' $i3config
 
 sed -i '/^[^#]*set \$mod Mod4/s/^/#/' $i3config
-sed -i 's/^bindsym Control+\$mod+Left exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh prev/#bindsym Control+\$mod+Left exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh prev/' $i3config
-sed -i 's/^bindsym Control+\$mod+Right exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh next/#bindsym Control+\$mod+Right exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh next/' $i3config
+sed -i 's/^bindsym Control+\$mod+Left exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh prev/#bindsym Control+\$mod+Left exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh prev/' $i3move
+sed -i 's/^bindsym Control+\$mod+Right exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh next/#bindsym Control+\$mod+Right exec \$HOME\/.config\/i3\/Scripts\/movetoWorkspace.sh next/' $i3move
 
 i3 reload
